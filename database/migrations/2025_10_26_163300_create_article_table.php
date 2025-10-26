de8587042eb1499d89dac5b03361eaf0a74288f4
AdeebAbdullah <myadeeb03@gmail.com>
2025-10-27 01:47:18 +0800

    baru migrate 2 table

---
+++ database/migrations/2025_10_26_163300_create_article_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->string('author_username');
+            $table->timestamps();
+
+            $table->foreign('author_username')
+                -> references('username')
+                -> on('users')
+                -> onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
